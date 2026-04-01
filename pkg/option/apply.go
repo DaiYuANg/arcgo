4523b9cfae86c2351c76728fd0c0e6ebdf86091a
ddddd <dai_yuang@icloud.com>
2026-04-01 10:53:41 +0800

    refactor option apply helper

--- pkg/option/apply.go
+++ pkg/option/apply.go
@@ -1,16 +1,14 @@
 package option
 
-import "github.com/samber/lo"
-
 // Apply executes non-nil option functions against the target in order.
 func Apply[T any, O ~func(*T)](target *T, opts ...O) {
 	if target == nil || len(opts) == 0 {
 		return
 	}
 
-	lo.ForEach(opts, func(opt O, _ int) {
+	for _, opt := range opts {
 		if opt != nil {
 			opt(target)
 		}
-	})
+	}
 }
